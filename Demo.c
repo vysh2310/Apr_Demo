06e9380eab1e6651f78e929047d332abb7e2f5c7
vysh2310 <vysh.simha@gmail.com>
2023-04-06 16:03:00 +0530

    XVZVZ

--- Demo.c
+++ Demo.c
@@ -2,13 +2,13 @@ int new_position(int sensor_pos1, int sensor_pos2)
 {
 int actuator_position;
 int x, y, tmp, magnitude;
-x=1;
+x=2;
 actuator_position = 2;  /* default*/
 tmp = 0;                        /* values */
 magnitude = sensor_pos1 / 100;
-y=1;
+y=magnitude + 2;
 
-while (actuator_position < 10)
+while (actuator_position < 100)
         {
         actuator_position++;
         y++;
@@ -16,12 +16,12 @@ while (actuator_position < 10)
     }
 if ((3*magnitude + 100) > 43)
         {
-
-        actuator_position = x / (x - y);
-    y = y + 1;
+        y = y + 1;
+       actuator_position = x / (x - y);
+    
         }
 return actuator_position*magnitude;     /* value */
 }
+
 int main(void){
-        new_position(-10,10);
-}
+        new_position(-100,100);}
