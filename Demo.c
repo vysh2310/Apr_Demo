5317c5272264461510a2baeeec96bb0bb9150935
vysh2310 <vysh.simha@gmail.com>
2023-04-06 12:08:24 +0530

    changed

--- Demo.c
+++ Demo.c
@@ -1,23 +1,24 @@
-int new_position(int sensor_pos1, int sensor_pos2) 
-{
-int actuator_position;
-int x, y, tmp, magnitude;
+#define false 0
+#define MAX_SIZE 10
+int tab[MAX_SIZE];
+int* arr = tab;
+static int interpolation(void);
+static int interpolation(void) {
+    int i, item = 0;
+    int found = false;
 
-actuator_position = 2; 	/* default*/
-tmp = 0;			/* values */
-magnitude = sensor_pos1 / 100;
-y = magnitude + 5;
 
-while (actuator_position < 10)
-	{
-	actuator_position++;
+    for (i = 0; i < 10; i++) {
+        arr++;
+        if ((found == false) && (*arr > 16)) {
+            found = 1;
+            item = i;
+        }
     }
-if ((3*magnitude + 100) > 43)
-	{
-	magnitude++;	
-	actuator_position = x / (x - y);
-    y = magnitude + 1;
- 	}
-return actuator_position*magnitude;	/* value */
+    *arr = 20;
+    return item;
 }
 
+int main(void){
+	interpolation();
+}
