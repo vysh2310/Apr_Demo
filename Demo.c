7f2fe6ad1f82e5ec1d266e5b17445438394dbef2
vysh2310 <vysh.simha@gmail.com>
2023-04-06 12:51:57 +0530

    new code

--- Demo.c
+++ Demo.c
@@ -1,21 +1,28 @@
-int where_are_the_errors(int input) {
-   int x,y,k;
+int new_position(int sensor_pos1, int sensor_pos2)
+{
+int actuator_position;
+int x, y, tmp, magnitude;
+x=1;
+actuator_position = 2;  /* default*/
+tmp = 0;                        /* values */
+magnitude = sensor_pos1 / 100;
+//y = magnitude + 5;
+y=1;
 
-   k = input / 100;
-   x = 2;
-   y = 1;
-   while (x < 10) {
-      x++;
-      y = y + 1;
-   }
+while (actuator_position < 10)
+        {
+        actuator_position++;
+        y++;
+        x++;
+    }
+if ((3*magnitude + 100) > 43)
+        {
 
-   if ((3*k + 100) > 43) {
-      y++;
-      x = x / (x - y);
-   }
-   return x;	
+        actuator_position = x / (x - y);
+    y = y + 1;
+        }
+return actuator_position*magnitude;     /* value */
 }
-
 int main(void){
-	where_are_the_errors(8);
+        new_position(-10,10);
 }
