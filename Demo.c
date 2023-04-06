760197952071650f2c16b1c739606e113da8b099
vysh2310 <48465529+vysh2310@users.noreply.github.com>
2023-04-06 09:45:17 +0530

    Created Source

---
+++ Demo.c
@@ -0,0 +1,23 @@
+int new_position(int sensor_pos1, int sensor_pos2,int position1) 
+{
+int actuator_position;
+int x, y, tmp, magnitude;
+
+actuator_position = 2; 	/* default*/
+tmp = 0;			/* values */
+magnitude = sensor_pos1 / 100;
+y = magnitude + 5;
+
+while (actuator_position < 10)
+	{
+	actuator_position++;
+     	}
+if ((3*magnitude + 100) > 43)
+	{
+	magnitude++;	
+	actuator_position = x / (x - y);
+    y = magnitude + 1;
+ 	}
+return actuator_position*magnitude;	/* value */
+}
+
