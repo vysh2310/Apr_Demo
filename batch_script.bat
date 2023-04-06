bacbfe9ee4bc6bfa47b4cb03fb110704c37c2a4c
vysh2310 <48465529+vysh2310@users.noreply.github.com>
2023-04-06 09:50:55 +0530

    Update batch_script.bat

--- batch_script.bat
+++ batch_script.bat
@@ -1,9 +1,9 @@
- echo "Polyspace analysis for any defects, do not commit or merge"
+ echo "Polyspace analysis showed defects, do not commit or merge"
  cd C:\Program Files\Polyspace\R2023a\polyspace\bin
- polyspace-bug-finder -sources C:\DebugGit\DebugMethod\Debugging.c -results-dir C:\DebugGit\DebugMethod\temp1_analysis_result
+ polyspace-bug-finder -sources C:\Apr_Demo\Apr_Demo\Demo.c -results-dir C:\Apr_Demo\Apr_Demo\temp3_analysis_result
  polyspace-results-export -format csv ^
-    -results-dir C:\DebugGit\DebugMethod\temp1_analysis_result ^
-    -output-name C:\DebugGit\DebugMethod\temp1_analysis_result.csv
+    -results-dir C:\DebugGit\DebugMethod\temp3_analysis_result ^
+    -output-name C:\DebugGit\DebugMethod\temp3_analysis_result.csv
  
 
 
